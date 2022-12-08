3c81ab0efe9b1f2a88a7039bb2ffbc67a9a68306
emurhowho efetobore hopewells <emurhopewell12@gmail.com>
2022-12-07 21:03:02 -0800

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,5 @@
-cutable File  5 lines (3 sloc)  148 Bytes
-
 #!/usr/bin/python3
 
 
 def print_sorted_dictionary(a_dictionary):
+[print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]

7df1572e5aed8dbc7e457816b4f9831263d6dcfa
emurhowho efetobore hopewells <emurhopewell12@gmail.com>
2022-12-08 18:07:02 -0800

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - Print all elements from a list
+ * @h: pointer to the star of a linked list
+ *
+ * Return: number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+}
+return (i);
+}
